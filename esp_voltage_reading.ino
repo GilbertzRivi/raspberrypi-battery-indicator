2cac5197ef40a4b1cf2fe573ec9e8d0fb283d48b
GilbertzRivi <62563806+GilbertzRivi@users.noreply.github.com>
2023-12-31 19:15:15 +0100

    Update esp_voltage_reading.ino

--- esp_voltage_reading.ino
+++ esp_voltage_reading.ino
@@ -1,5 +1,5 @@
 const int analogInPin = A0;  // Analog Pin ADC0 = A0
-const float x = 0,0042715484363082; // tutorial how to adjust this value in README.md
+const float x = 0.0041435479765844; // tutorial how to adjust this value in README.md
 float min_voltage = 3.1; // I want my esp to repport 0% when avarage voltage on a cell drops to 3.1V, ajust it to yours liking
 float max_voltage = 4.2; // Voltage of max charged batteries
 int number_of_cells = 4; // If you use 4s battery put here 4, if 3s - 3 etc
