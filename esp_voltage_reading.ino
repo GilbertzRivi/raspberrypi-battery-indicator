c3c344e9c045d2b9fd738b9e683b40637cc75b42
GilbertzRivi <62563806+GilbertzRivi@users.noreply.github.com>
2023-12-31 18:15:12 +0100

    Update esp_voltage_reading.ino

--- esp_voltage_reading.ino
+++ esp_voltage_reading.ino
@@ -1,8 +1,10 @@
 const int analogInPin = A0;  // Analog Pin ADC0 = A0
-const float x = 0.4172099087353325; // adjust this number to match your microcontrollers ADC using formula = ADC-battery-reading/battery-voltage-in-milivolts
+const float x = 0.4172099087353325; // adjust this number to match your microcontrollers ADC using formula = ADC-battery-reading/(battery-voltage*100)
+float min_voltage = 3.1; // I want my esp to repport 0% when avarage voltage on a cell drops to 3.1V, ajust it to yours liking
+float max_voltage = 4.2; // Voltage of max charged batteries
+int number_of_cells = 4; // If you use 4s battery put here 4, if 3s - 3 etc
 float v = 0;
 float percent = 0;
-
 int sensorValue = 0;  // value read from the pot
 
 void setup() {
@@ -11,9 +13,9 @@ void setup() {
 
 void loop() {
   sensorValue = analogRead(analogInPin);
-  v = sensorValue * x / 100;
-  percent = ((v / 4) - 3) / 1.2;
+  v = sensorValue * x / 100; // v represents now voltage of your battery pack
+  percent = ((v / number_of_cells) - min_voltage) / (max_voltage - min_voltage);
   Serial.println(percent);
-  // this delay has to be the same as in your python script
-  delay(1000);
+  // this delay has to be the same as in your python script, note that delay here is in ms but in python it is in s, so if here is 30000 in python it should be 30
+  delay(30000);
 }
